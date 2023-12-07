8dbbed44f1698bca2c724cf818ad314d05d76229
LolKyy <98279949+LolKyy@users.noreply.github.com>
2023-12-07 20:11:26 +0300

    Commit
    Commit

---
+++ Congrats2/Controllers/CongratsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Congrats2.Controllers
+{
+    public class CongratsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
